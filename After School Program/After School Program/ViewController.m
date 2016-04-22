0a57a8bba6e9cec1f17a1e31dd43d182468404ae
Clyff Millet <clyffm@gmail.com>
2016-04-22 15:47:50 -0400

    after school program

---
+++ After School Program/After School Program/ViewController.m
@@ -0,0 +1,58 @@
+//
+//  ViewController.m
+//  After School Program
+//
+//  Created by Clyfford Millet on 4/22/16.
+//  Copyright © 2016 Clyff IOS supercompany. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    Ali Forney Center
+    http://www.aliforneycenter.org/
+    
+    Covenant House NY
+    https://ny.covenanthouse.org/
+    
+    Safe space NYC
+    http://www.safespacenyc.org/safespace/?
+    
+    Rachels place
+    http://www.rachelsplaceproductions.org/
+    
+    green chimneys
+    http://www.greenchimneys.org/
+    
+    girls educational and mentoring services
+    http://www.gems-girls.org/
+    
+    Bronx community pride center
+    http://blog.bronxlgbtqcenter.org/
+    
+    SCO drop in center
+    http://sco.org/
+    
+    the turning Point
+    http://tpny.org/
+    
+    
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
